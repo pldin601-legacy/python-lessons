8bbf214c3ccd26255865a2c7980150971b1dcfa5
pldin601 <roman@homefs.biz>
2015-07-13 19:11:38 +0300

    Trees on Python.

--- trees.py
+++ trees.py
@@ -2,21 +2,17 @@ __author__ = 'roman'
 
 
 def partition(num):
-    return [[num]] + [[x] + y for x in range(1, num)
-                      for y in partition(num - x)]
+    return list(filter(lambda v: match_pairs(v, lambda x, y: x < y),
+                       [[num]] + [[x] + y for x in range(1, num) for y in partition(num - x)]))
 
 
-def rec_range(gen_min, gen_max, step=1):
-    print(gen_min)
-    if gen_min + step < gen_max:
-        rec_range(gen_min + step, gen_max)
+def match_pairs(coll, comp):
+    if len(coll) < 2:
+        return True
+    elif comp(coll[0], coll[1]):
+        return match_pairs(coll[1::], comp)
+    else:
+        return False
 
 
-def trees(total, need):
-    max_step = int(total / need) + 1
-    width = lambda step, count: (count - 1) * step + 1
-    inclusions = lambda step: total - width(step, need) + 1
-    return sum(inclusions(s) for s in range(1, max_step))
-
-
-print(trees(6, 3))
+print(partition(10))
