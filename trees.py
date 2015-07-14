f3bd73720bba34d84d40d7bb5824807541f57b9d
pldin601 <roman@homefs.biz>
2015-07-14 11:17:40 +0300

    "Stairs from Cubes"

--- trees.py
+++ trees.py
@@ -8,23 +8,19 @@ def partition(num):
             yield [x] + y
 
 
-def match_pairs(coll, comp):
+def match_pairs(coll):
     if len(coll) < 2:
         return True
     for a in range(len(coll) - 1):
-        if not comp(coll[a], coll[a + 1]):
+        if coll[a] >= coll[a + 1]:
             return False
     return True
 
 
-def compare(x, y):
-    return x < y
-
-
 def find_trees(num):
     for t in partition(num):
-        if match_pairs(t, compare):
+        if match_pairs(t):
             print(t)
 
 
-find_trees(10)
+find_trees(14)
